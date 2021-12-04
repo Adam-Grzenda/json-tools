06a5795eae5ee5b9728df86867c20afce6a92e5c
Adam-Grzenda <65490875+Adam-Grzenda@users.noreply.github.com>
2021-12-05 00:07:47 +0100

    Add convenience dependency for lombok

--- src/main/java/pl/put/poznan/controller/JsonToolsController.java
+++ src/main/java/pl/put/poznan/controller/JsonToolsController.java
@@ -1,7 +1,7 @@
 package pl.put.poznan.controller;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 import pl.put.poznan.service.JsonService;
 
@@ -10,22 +10,17 @@ import java.util.Arrays;
 
 @RestController
 @RequestMapping("/{text}")
+@RequiredArgsConstructor @Slf4j
 public class JsonToolsController {
 
-    private static final Logger logger = LoggerFactory.getLogger(JsonToolsController.class);
     private final JsonService jsonService;
-
-    public JsonToolsController(JsonService jsonService) {
-        this.jsonService = jsonService;
-    }
-
 
     @RequestMapping(method = RequestMethod.GET, produces = "application/json")
     public String get(@PathVariable String text,
                               @RequestParam(value="transforms", defaultValue="upper,escape") String[] transforms) {
 
-        logger.debug(text);
-        logger.debug(Arrays.toString(transforms));
+        log.debug(text);
+        log.debug(Arrays.toString(transforms));
 
         return jsonService.transform(text);
     }
@@ -34,8 +29,8 @@ public class JsonToolsController {
     public String post(@PathVariable String text,
                       @RequestBody String[] transforms) {
 
-        logger.debug(text);
-        logger.debug(Arrays.toString(transforms));
+        log.debug(text);
+        log.debug(Arrays.toString(transforms));
 
         return jsonService.transform(text);
     }
