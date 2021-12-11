940ab175de62b40d9b87da19eb30620abb63d970
llkadenll <kdwornik@o2.pl>
2021-12-11 19:55:23 +0100

    Create minify and update deminify tools

--- src/main/java/pl/put/poznan/controller/JsonToolsController.java
+++ src/main/java/pl/put/poznan/controller/JsonToolsController.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 import pl.put.poznan.service.JsonDeminify;
+import pl.put.poznan.service.JsonMinify;
 
 import java.util.Arrays;
 
@@ -13,7 +14,15 @@ import java.util.Arrays;
 @RequiredArgsConstructor @Slf4j
 public class JsonToolsController {
 
+    private final JsonMinify jsonMinify;
     private final JsonDeminify jsonDeminify;
+
+    @RequestMapping(value = "/minify", method = RequestMethod.POST, produces = "application/json")
+    public String minify(@RequestBody String text) {
+        log.debug(text);
+
+        return jsonMinify.minify(text);
+    }
 
     @RequestMapping(value = "/deminify", method = RequestMethod.POST, produces = "application/json")
     public String deminify(@RequestBody String text) {
