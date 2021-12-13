39c9027bc0e7b934a8ecd750418829cca4d50682
Mikolaj Burdzinski <mikolaj.burdzinski@student.put.poznan.pl>
2021-12-13 21:49:10 +0100

    Add missing import

--- src/main/java/pl/put/poznan/controller/JsonToolsController.java
+++ src/main/java/pl/put/poznan/controller/JsonToolsController.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import pl.put.poznan.service.JsonDeminify;
