5b64ada90f696ca6fa70bed59ded83343a350481
Mikolaj Burdzinski <mikolaj.burdzinski@student.put.poznan.pl>
2021-12-13 18:41:19 +0100

    Add delete implementation

---
+++ src/main/java/pl/put/poznan/service/JsonDelete.java
@@ -0,0 +1,24 @@
+package pl.put.poznan.service;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.bohnman.squiggly.Squiggly;
+import com.github.bohnman.squiggly.util.SquigglyUtils;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JsonDelete {
+    ObjectMapper mapper = new ObjectMapper();
+
+    public String delete(String text, String[] fields) {
+        try {
+            String filters = "-" + String.join(",-", fields);
+
+            Object json = mapper.readValue(text, Object.class);
+            mapper = Squiggly.init(new ObjectMapper(), filters);
+            return SquigglyUtils.stringify(mapper, json);
+        } catch (JsonProcessingException e) {
+            return "";
+        }
+    }
+}
